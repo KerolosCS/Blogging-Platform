157cef65b95516fdce726a0fc39e6142bbbab820
KerolosCS <kerolosfady4@gmail.com>
2024-08-07 19:00:17 +0300

    Final Validation On Email and password

--- testTask/DTOs/UserCreate.cs
+++ testTask/DTOs/UserCreate.cs
@@ -10,6 +10,8 @@ namespace testTask.DTOs
         [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
         [Required]
+        [StringLength(100, MinimumLength = 7, ErrorMessage = "Password must be at least 7 characters long.")]
+        [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{7,}$", ErrorMessage = "Password must be at least 7 characters long, and contain at least one letter, one number, and one special character.")]
         public string Password { get; set; }
     }
 }
